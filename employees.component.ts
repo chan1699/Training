91fe2caea93b33319f62084ff852074aeab35e3d
chan1699 <80826265+chan1699@users.noreply.github.com>
2021-05-14 21:32:19 +0530

    Add files via upload

---
+++ employees.component.ts
@@ -0,0 +1,10 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-employees',
+  templateUrl: './employees.component.html',
+  styleUrls: ['./employees.component.css']
+})
+export class EmployeesComponent {
+ employee ={name:'Anoop',Age:22,location:'Hyderabad'}
+}
